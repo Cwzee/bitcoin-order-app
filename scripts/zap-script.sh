67749a4cce3d78cdcca5c794d079cba88843a08c
Chee Weii Zee <d1358097@ncsdevnet.com>
2023-11-28 16:29:42 +0800

    lint, dast,sast

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -2,7 +2,7 @@
 
 docker pull owasp/zap2docker-stable
 docker run -i owasp/zap2docker-stable zap-baseline.py -t 
-"https://Cwzee.github.io/bitcoin-order-app/" -l PASS > 
+"https://kenken64.github.io/bitcoin-order-app/" -l PASS > 
 zap_baseline_report.html
 
 echo $? > /dev/null
